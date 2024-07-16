a7d4b881ccaf191ad5760c3869562b52420d54e2
Anup Saud <anupsaud10@gmail.com>
2024-07-16 08:22:38 -0400

    Initial Commit

---
+++ JP Apex Predators/JP_Apex_PredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JP_Apex_PredatorsApp.swift
+//  JP Apex Predators
+//
+//  Created by Anup Saud on 2024-07-16.
+//
+
+import SwiftUI
+
+@main
+struct JP_Apex_PredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
